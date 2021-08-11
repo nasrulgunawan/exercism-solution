9b31dc4fc3742c656b541fee5bd4b62c7dedb068
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-08-11 23:23:34 +0700

    Fixed some failed test

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,25 +1,28 @@
 defmodule Bob do
   def hey(sentence) do
     cond do
-      question?(sentence) ->
-        "Sure."
-      yell?(sentence) ->
-        "Woah, chill out!"
-      silence?(sentence) ->
-        "Fine. Be that way!"
+      question?(sentence) -> "Sure."
+      silence?(sentence) -> "Fine. Be that way!"
+      yell?(sentence) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
 
+  defp normalize(sentence) do
+    sentence
+      |> String.replace(~r/,\s[^[:alpha:][:blank:]['?']]/u, "")
+      |> String.trim
+  end
+
   defp question?(sentence) do
-    String.last(sentence) === "?"
+    String.last(normalize(sentence)) == "?"
   end
 
   defp yell?(sentence) do
-    String.upcase(sentence) === sentence
+    String.upcase(normalize(sentence)) === normalize(sentence)
   end
 
   defp silence?(sentence) do
-    String.trim(sentence) === ""
+    normalize(sentence) == ""
   end
 end
