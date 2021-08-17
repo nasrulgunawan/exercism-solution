c3d9dd74dfd5fad5a8da7e233c8b5420dbd9add5
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-08-17 21:01:15 +0700

    Add clock - ruby solution

--- bonus-challenges/clock/clock.rb
+++ bonus-challenges/clock/clock.rb
@@ -6,3 +6,31 @@ To get started with TDD, see the `README.md` file in your
 `ruby/clock` directory.
 =end
 
+class Clock
+  attr_reader :hour, :minute
+
+  def initialize(hour: 0, minute: 0)
+    @hour = (hour + minute / 60) % 24 
+    @minute = minute % 60
+  end
+
+  def +(other)
+    hour = self.hour + other.hour
+    minute = self.minute + other.minute
+    Clock.new(hour: hour, minute: minute)
+  end
+
+  def -(other)
+    hour = self.hour - other.hour
+    minute = self.minute - other.minute
+    Clock.new(hour: hour, minute: minute)
+  end
+
+  def ==(other)
+    hour == other.hour && minute == other.minute
+  end
+
+  def to_s
+    "%02d:%02d" % [hour, minute]
+  end
+end
